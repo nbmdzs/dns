2c630bbe6fc5eb117a0f09b94ad67a15fefc198e
fyr77 <login@notjakob.com>
2020-11-10 15:15:01 +0100

    fix cookies

--- js/tool.js
+++ js/tool.js
@@ -21,7 +21,7 @@ function deleteAllCookies() {
         var cookie = cookies[i];
         var eqPos = cookie.indexOf("=");
         var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT; SameSite=Strict; Secure";
     }
 }
 
